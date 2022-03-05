f57ef9e719305fafc4240e9a8985c6e2f3354151
Adamenko-Vladislav <vladadam20@gmail.com>
2022-03-05 15:39:28 +0300

    Task5-Adamenko-Titliguzov

---
+++ sem-2/Task5-Adamenko-Titliguzov/main.cpp
@@ -0,0 +1,10 @@
+#include <QApplication>
+
+#include "main_window.h"
+
+int main(int argc, char* argv[]) {
+  QApplication a(argc, argv);
+  main_window window;
+  window.show();
+  return QApplication::exec();
+}
