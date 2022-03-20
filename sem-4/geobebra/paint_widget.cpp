28bf89f9c937dffb071e21372c9adf20d283fda3
DudkoAndrei <dudkoandrei@gmail.com>
2022-03-20 21:54:16 +0300

    Fix drawing order

--- sem-4/geobebra/paint_widget.cpp
+++ sem-4/geobebra/paint_widget.cpp
@@ -9,16 +9,8 @@ void PaintWidget::Paint(
     const QColor& plot_color,
     const std::vector<QPoint>& points) {
   painter->save();
-  painter->setPen(plot_color);
-  QPainterPath path;
-  path.moveTo(points.front());
-  for (const auto& point : points) {
-    path.lineTo(point);
-  }
 
   painter->translate(QWidget::width() / 2, QWidget::height() / 2);
-
-  painter->drawPath(path);
 
   QPainterPath axis_path;
   axis_path.moveTo(0, -QWidget::height() / 2);
@@ -29,6 +21,18 @@ void PaintWidget::Paint(
   QColor axis_color = Qt::black;
   painter->setPen(axis_color);
   painter->drawPath(axis_path);
+
+
+  painter->setPen(plot_color);
+  QPainterPath path;
+  path.moveTo(points.front());
+  for (const auto& point : points) {
+    path.lineTo(point);
+  }
+
+
+  painter->drawPath(path);
+
 
   painter->restore();
 }
