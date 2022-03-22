aa68235e573f31d02fa84e7b606707f3d92ceb4b
elizabethfeden <elizabethfeden@gmail.com>
2022-03-22 15:34:57 +0300

    Fix model

--- lectures/mvp-demo/model.h
+++ lectures/mvp-demo/model.h
@@ -8,11 +8,15 @@ class Model {
  public:
   const QString& Element(size_t i) const { return queue_[i]; }
   const size_t Size() const { return queue_.size(); }
-  void Pop() { queue_.erase(queue_.begin()); }
+  
+  void Pop() {
+    if (!queue_.empty()) {
+      queue_.erase(queue_.begin());
+    }
+  }
   void Push(const QString& item) { queue_.push_back(item); }
  private:
   std::vector<QString> queue_;
 };
-
 
 #endif // MODEL_H
