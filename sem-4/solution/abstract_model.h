16f3a14f3aadd99466264a0206e585fae3bb60ad
Elizabeth <52410353+elizabethfeden@users.noreply.github.com>
2022-03-25 22:12:39 +0300

    Add dictionaries

--- sem-4/solution/abstract_model.h
+++ sem-4/solution/abstract_model.h
@@ -3,7 +3,7 @@
 
 /*
  * Model's available dictionaries list is constant. The dictionaries are stored
- * in app's resources. Every dictionary has id, and name; every dictionary file
+ * in app's resources. Every dictionary has id and name; every dictionary file
  * has list of words in it, separated by newline.
  * User may choose to make certain dictionaries inactive. The FindWord method
  * should only check if a word is present in active dictionaries.
@@ -12,10 +12,20 @@
 class AbstractModel {
  public:
   virtual std::unordered_map<size_t, QString>
-      GetAvailableDictionaries() const = 0;
+      GetAvailableDictionaries() const { return kDictionaryNames_; }
   virtual void SetActiveDictionaries(
       const std::vector<size_t>& active_dictionary_ids) = 0;
   virtual bool FindWordInDictionaries(const QString& word) = 0;
+
+ private:
+  const std::unordered_map<size_t, QString> kDictionaryNames_ = {
+      {1, "english"},
+      {2, "russian"},
+      {3, "adjectives"},
+      {4, "adverbs"},
+      {5, "nouns"},
+      {6, "verbs"}
+  };
 };
 
 #endif // ABSTRACT_MODEL_H
