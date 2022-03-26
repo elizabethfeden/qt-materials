7dc545475c7141f26c0b295ff0b35ace544ffb7e
Iren137 <99528522+Iren137@users.noreply.github.com>
2022-03-26 14:28:03 +0300

    Add solution of View

--- sem-4/solution/views.h
+++ sem-4/solution/views.h
@@ -10,8 +10,21 @@
 // Implement the views.
 
 class ConsoleView : public AbstractView {
+ public:
+  explicit ConsoleView(std::unique_ptr<Controller> controller);
   // Returns false if command is '#exit'.
   bool ParseCommand(const std::string& command);
+
+  QString GetFilenameToLoad() override;
+  void SetText(const QString& text) override;
+  QString GetText() override;
+  std::vector<size_t> GetActiveDictionaryIds() override;
+  void ShowResults(const std::vector<QString>& results) override;
+
+ private:
+  QString text_;
+  QString filename_;
+  std::vector<size_t> dictionary_ids_;
 };
 
 class WindowView : public AbstractView, public QMainWindow {
