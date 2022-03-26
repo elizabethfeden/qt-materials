75557709725fc746b3e9a466e5082ac3bf9f7678
Marmotikon <99591040+Marmotikon@users.noreply.github.com>
2022-03-26 10:54:26 +0300

    Add Model

--- sem-4/solution/models.cpp
+++ sem-4/solution/models.cpp
@@ -1,1 +1,15 @@
 #include "models.h"
+void HashModel::SetActiveDictionaries(const std::vector<size_t>& active_dictionary_ids) {
+  dictionaries_ = active_dictionary_ids;
+}
+bool HashModel::FindWordInDictionaries(const QString& word) {
+
+}
+HashModel::HashModel() {
+  std::vector<size_t>& active_dicts;
+}
+void HashModel::GetWords(const QString& name) {
+  QSet<QString> current_words;
+
+  words_.push_back(std::move(current_words));
+}
