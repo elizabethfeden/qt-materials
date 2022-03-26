87aed578355f6b78de2b4513ab1bfaea0a9f2fde
Marmotikon <msemenov93057@gmail.com>
2022-03-26 10:52:47 +0300

    Add Model

--- sem-4/solution/models.h
+++ sem-4/solution/models.h
@@ -2,19 +2,29 @@
 #define MODELS_H
 
 #include "abstract_model.h"
-
+#include <vector>
+#include <QString>
+#include <QSet>
 // -- TASK --
 // Implement the models. HashModel should store words entries in an
 // unordered set or map. TrieModel should implement search of words entries
 // with the Trie data structure.
 
 class HashModel : public AbstractModel {
+ public:
+  HashModel();
+  void SetActiveDictionaries(const std::vector<size_t>& active_dictionary_ids) override;
+  bool FindWordInDictionaries(const QString& word) override;
 
+ private:
+  void GetWords(const QString& name);
+
+  std::vector<size_t> dictionaries_;
+  std::vector<QSet<QString>> words_;
 };
 
 class TrieModel : public AbstractModel {
 
 };
-
 
 #endif // MODELS_H
