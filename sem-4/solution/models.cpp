bf8d1e7b39a540a2853157f27e9c10877ba6026b
niki4smirn <kitos.smirnov@gmail.com>
2022-04-02 09:33:49 +0300

    Add TrieModel implementation

--- sem-4/solution/models.cpp
+++ sem-4/solution/models.cpp
@@ -32,3 +32,65 @@ void HashModel::LoadDictionary(const QString& dictionary_name) {
   file.close();
   words_.push_back(std::move(current_words));
 }
+
+void TrieModel::LoadDictionary(
+    const std::pair<size_t, QString>& dictionary_info) {
+  QFile file(":dictionaries/" + dictionary_info.second + ".txt");
+  file.open(QFile::ReadOnly);
+  while (!file.atEnd()) {
+    AddWord(QString::fromUtf8(file.readLine().toLower()),
+            dictionary_info.first);
+  }
+  file.close();
+}
+
+void TrieModel::AddWord(const QString& word, size_t dictionary_index) {
+  TrieNode* cur_node = root_;
+  for (auto character : word) {
+    auto& next = cur_node->next;
+    auto it = next.find(character);
+    if (it != next.end()) {
+      cur_node = it->second;
+    } else {
+      next[character] = new TrieNode(character);
+      cur_node = next[character];
+    }
+  }
+  cur_node->dictionaries.push_back(dictionary_index);
+}
+
+TrieModel::TrieModel() : root_(new TrieNode()) {
+  for (auto& entry : GetAvailableDictionaries()) {
+    LoadDictionary(entry);
+  }
+}
+void TrieModel::SetActiveDictionaries(
+    const std::vector<size_t>& active_dictionary_ids) {
+  active_dictionaries_ = active_dictionary_ids;
+}
+
+bool TrieModel::FindWordInDictionaries(const QString& word) {
+  TrieNode* cur_node = root_;
+  for (auto character : word) {
+    auto& next = cur_node->next;
+    auto it = next.find(character);
+    if (it == next.end()) {
+      return false;
+    }
+    cur_node = it->second;
+  }
+  auto& cur_dicts = cur_node->dictionaries;
+  return std::ranges::any_of(cur_dicts.begin(),
+                             cur_dicts.end(),
+                             [&](size_t cur_dict_index) {
+                               if (std::find(active_dictionaries_.begin(),
+                                             active_dictionaries_.end(),
+                                             cur_dict_index)
+                                   != active_dictionaries_.end()) {
+                                 return true;
+                               }
+                               return false;
+                             });
+}
+
+TrieModel::TrieNode::TrieNode(QChar value) : value(value) {}
--- sem-4/solution/models.h
+++ sem-4/solution/models.h
@@ -26,7 +26,27 @@ class HashModel : public AbstractModel {
 };
 
 class TrieModel : public AbstractModel {
+ public:
+  TrieModel();
+  void SetActiveDictionaries(
+      const std::vector<size_t>& active_dictionary_ids) override;
+  bool FindWordInDictionaries(const QString& word) override;
 
+ private:
+  void LoadDictionary(const std::pair<size_t, QString>& dictionary_info);
+  void AddWord(const QString& word, size_t dictionary_index);
+  std::vector<size_t> active_dictionaries_;
+
+  struct TrieNode {
+    TrieNode() = default;
+    explicit TrieNode(QChar value);
+
+    std::map<QChar, TrieNode*> next;
+    QChar value;
+    std::vector<size_t> dictionaries;
+  };
+
+  TrieNode* root_;
 };
 
 #endif // MODELS_H
