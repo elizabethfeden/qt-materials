df5e6fd4c585a36bde1f7a4b5e36df65960eb1e4
AdamenkoVladislav <48858969+Adamenko-Vladislav@users.noreply.github.com>
2022-04-09 20:58:33 +0300

    Sem-5 Adamenko Kharasov

---
+++ sem-5/translations/by.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="be" sourcelanguage="en">
+<context>
+    <name>QLabel</name>
+    <message>
+        <location filename="../main_window.cpp" line="8"/>
+        <source>Cake is a lie</source>
+        <translation>Торт гэта хлусня</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="9"/>
+        <source>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</source>
+        <translation>Боль сам па сабе - гэта любоў да болю, асноўныя экалагічныя праблемы, але я даю такі час, каб упасці, так што нейкі вялікі боль і боль. Каб звесці да мінімуму, хто з нашых павінен займацца якой-небудзь працай, акрамя таго, каб скарыстацца наступствамі гэтага. Але боль у фільме незразумела асуджаць, у задавальненні ён хоча ўцячы ад болю быць cillum у болю, без выніку. Гэта тыя выключэння, якіх прагнуць сляпы, яны не бачаць, яны адмаўляюцца ад сваіх абавязкаў на віну, якая супакойвае душэўныя цяжкасці.</translation>
+    </message>
+</context>
+<context>
+    <name>QPushButton</name>
+    <message>
+        <location filename="../main_window.cpp" line="11"/>
+        <source>Press me</source>
+        <translation>Прыцісні мяне</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="12"/>
+        <source>Do not press me...</source>
+        <translation>Не ціснуць на мяне...</translation>
+    </message>
+</context>
+</TS>
---
+++ sem-5/translations/en.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="en">
+<context>
+    <name>QLabel</name>
+    <message>
+        <location filename="../main_window.cpp" line="8"/>
+        <source>Cake is a lie</source>
+        <translation>Cake is a lie</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="9"/>
+        <source>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</source>
+        <translation>The pain itself is the love of the pain, the main ecological problems, but I give this kind of time to fall down, so that some great pain and pain. For the purpose of coming to a minimum, who of ours should exercise any employment except to take advantage of the consequences from it. But the pain in the film is irure to condemn, in the pleasure it wants to escape from the pain of being clum in pain, none result. They are the exceptions the blinds yearn for, they do not see, they are the ones who abandon their responsibilities to the fault that is soothing the soul&apos;s hardships.</translation>
+    </message>
+</context>
+<context>
+    <name>QPushButton</name>
+    <message>
+        <location filename="../main_window.cpp" line="11"/>
+        <source>Press me</source>
+        <translation>Press me</translation>
+    </message>
+    <message>
+        <location filename="../main_window.cpp" line="12"/>
+        <source>Do not press me...</source>
+        <translation>Do not press me...</translation>
+    </message>
+</context>
+</TS>
